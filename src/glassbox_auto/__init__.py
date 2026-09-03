412caa1e979aa24e625b1d5e6dc03414eca204d2
BBSupreme <mikkelbredsgaard@gmail.com>
2026-09-03 20:42:17 +0200

    Expose package version 0.1.0

--- src/glassbox_auto/__init__.py
+++ src/glassbox_auto/__init__.py
@@ -1,9 +1,12 @@
+__version__ = "0.1.0"
+
 from .economics import PurchaseMethodBlockedError, lease_economics
 from .engine import evaluate_candidate, rank_candidates
 from .models import *
 from .scoring import evaluate_gate, piecewise_utility, score_candidate
 
 __all__ = [
+    "__version__",
     "PurchaseMethodBlockedError",
     "lease_economics",
     "evaluate_candidate",
